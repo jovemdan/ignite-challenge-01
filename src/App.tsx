2e6fbf62623fb55b1e2824b877819ff845173623
jovemdan <danilogdev@gmail.com>
2022-04-05 21:48:52 -0300

    fix(adjusts): identação

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { TaskList } from './components/TaskList'
-import { Header } from "./components/Header";
+import { Header } from "./components/Header"
 import './styles/global.scss'
-
-
 export function App() {
   return (
     <>
